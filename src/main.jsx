1bbcee0ee0be093dc24d70aeae5a241ac6754070
Melvin J Bonner <140230755+jb24000@users.noreply.github.com>
2025-08-12 00:17:03 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
+if (-not (Test-Path "C:\Code\REInvestor\src\main.jsx")) {
+@"
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
-
 createRoot(document.getElementById('root')).render(<App />)
+"@ | Set-Content -Path "C:\Code\REInvestor\src\main.jsx" -Encoding UTF8
+}
