3550273ea405017f93effa63da1d977ee3488cf5
Melvin J Bonner <140230755+jb24000@users.noreply.github.com>
2025-08-12 00:40:20 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// IMPORTANT: base must match your repo name for GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/REInvestorTool/', // important for GitHub Pages
+  base: '/REInvestorTool/',   // production base path on Pages
 })
